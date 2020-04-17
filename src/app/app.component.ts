3c84fd5e33a46af1098448160804799d1423fa33
chenwei liu <a500667337@gmail.com>
2020-04-17 07:47:53 +0000

    功能完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includesSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const paredValue = parseInt(value);
+
+    if (!isNaN(paredValue)) {
+      this.length = paredValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includesSymbols = !this.includesSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '123456789';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includesSymbols) {
+      validChars += symbols;
+    }
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onButtonClear() {
+    this.password = '';
+  }
 }
